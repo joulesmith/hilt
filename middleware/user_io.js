5f7c42701a0b2bec0c774bf64e62163731f0b06a
kcdodd <carter.dodd@gmail.com>
2016-04-12 19:55:21 -0500

    io authentication catch errors

--- middleware/user_io.js
+++ middleware/user_io.js
@@ -4,30 +4,35 @@ var mongoose = require('mongoose');
 
 module.exports = function(socket) {
   var User = mongoose.model("user");
-  
+
   socket.on('authenticate', function(data) {
-    var token = JSON.parse(new Buffer(data.authorization, 'base64').toString('utf8'));
 
-    if (token._id && token.secret) {
-      User.findById(token._id).exec()
-        .then(function(user) {
-          if (!user) {
-            throw 1;
-          }
+    try{
+      var token = JSON.parse(new Buffer(data.authorization, 'base64').toString('utf8'));
 
-          return user.verifyToken(token);
-        })
-        .then(function(user) {
-          if (!user) {
-            throw 1;
-          }
+      if (token._id && token.secret) {
+        User.findById(token._id).exec()
+          .then(function(user) {
+            if (!user) {
+              throw 1;
+            }
 
-          socket.user = user;
+            return user.verifyToken(token);
+          })
+          .then(function(user) {
+            if (!user) {
+              throw 1;
+            }
 
-        })
-        .catch(function(error) {
-          socket.disconnect('unauthorized');
-        });
+            socket.user = user;
+
+          })
+          .catch(function(error) {
+            socket.disconnect('unauthorized');
+          });
+      }
+    }catch(error){
+      socket.disconnect('unauthorized');
     }
   });
 };
